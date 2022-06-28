de35c6d350c001c6d97ed50b5e40ea843219df43
Nathan <fnathan@hotmail.fr>
2022-06-28 12:34:18 +0200

    basic client

--- Client/src/main.rs
+++ Client/src/main.rs
@@ -1,3 +1,17 @@
+use std::io::Write;
+use std::net::TcpStream;
+
 fn main() {
-    println!("Hello, world!");
+    println!("client starting...");
+
+    let mut stream = TcpStream::connect("localhost:7878");
+    match stream {
+        Ok(mut stream) => {
+            println!("connected to server");
+            let message = "Hello".as_bytes();
+            let response = stream.write_all(&message);
+
+        }
+        Err(err) => panic!("Failed to connect to address : {err:?}")
+    }
 }
