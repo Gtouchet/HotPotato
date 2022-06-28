64528d00d689c3e8761f76bae90bb4b8693933de
Nathan <fnathan@hotmail.fr>
2022-06-28 12:34:26 +0200

    basic server

--- Server/src/main.rs
+++ Server/src/main.rs
@@ -1,3 +1,19 @@
+use std::net::{SocketAddr, TcpListener};
+
 fn main() {
-    println!("Hello, world!");
-}
+    println!("server starting...");
+
+    // Create a new TCP listener on the given address.
+    let address = SocketAddr::from(([127, 0, 0, 1], 7878));
+    let listener = TcpListener::bind(address);
+
+    let listener = match listener {
+        Ok(l) => l,
+        Err(err) => panic!("Failed to bind to address : {err:?}")
+    };
+    
+    // Showing any message sent from client to server.
+    for message in listener.incoming() {
+        println!("message={message:?}");
+    }
+}   
