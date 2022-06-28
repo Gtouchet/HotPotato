42bb9a49543ac85745d38169ac230c21ad0fc04a
GTouchet <guillaumetouchet921@gmail.com>
2022-06-28 20:21:53 +0200

    it works

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,14 +1,24 @@
-use std::io::Write;
+use std::io::{Read, Write};
 use std::net::TcpStream;
+use serde::{Serialize, Deserialize};
+use serde_json;
 
 fn main() {
     let mut stream = TcpStream::connect("localhost:7878");
     match stream {
         Ok(mut stream) => {
-            let message= b"Hello";
-            let response = stream.write_all(message);
-            print!("resp: {:?}", response);
+            let test = send_message("\"Hello\"", stream);
+            print!("{}", test);
         }
-        Err(err) => panic!("Cannot connect : {err}"),
+        Err(err) => panic!("Failed to connect to address : {err:?}")
     }
+}
+
+fn send_message(message: &str, mut stream: TcpStream) -> String {
+    let message_size = message.len() as u32;
+    stream.write_all(&message_size.to_be_bytes());
+    stream.write_all(message.as_bytes());
+    let mut response = String::new();
+    stream.read_to_string(&mut response).unwrap();
+    response
 }
