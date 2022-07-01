1cb16f2a9a7e2ba12bdd23769f185d2bda58dac3
Nathan <fnathan@hotmail.fr>
2022-07-01 08:28:21 +0200

    PublicLeaderBoard fix

--- client/src/messages.rs
+++ client/src/messages.rs
@@ -6,7 +6,7 @@ pub enum Message {
     Welcome(Welcome),
     Subscribe(Subscribe),
     SubscribeResult(SubscribeResult),
-    PublicLeaderBoard(PublicLeaderBoard),
+    PublicLeaderBoard(Vec<PublicPlayer>),
     Challenge(Challenge),
     ChallengeResult(ChallengeResult),
     RoundSummary(RoundSummary),
